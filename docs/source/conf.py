48687aeb8320981d62662a3262d52fee2a829d3f
gerome.vivar <gerome.vivar@helmholtz-muenchen.de>
2022-04-20 11:46:35 +0200

    docs: fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,6 @@ import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
 from pathlib import Path
-import sphinx_rtd_theme
 
 # Add parent dir to known paths
 p = Path(__file__).parents[2]
