fcf6b6fd044037e62e48997e2fe1be87cbf938c8
gerome.vivar <gerome.vivar@helmholtz-muenchen.de>
2022-04-20 11:57:05 +0200

    docs: fix build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -77,3 +77,5 @@ html_logo = "_static/doku_icon.001.png"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
